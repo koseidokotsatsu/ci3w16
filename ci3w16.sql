3c10db6b65f4ab4ff43db74db826f591005897c2
unknown <surengpati5@gmail.com>
2023-11-15 15:33:38 +0700

    Membuat form stock in

--- ci3w16.sql
+++ ci3w16.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Nov 2023 pada 08.43
+-- Waktu pembuatan: 15 Nov 2023 pada 09.32
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.1.17
 
@@ -91,7 +91,17 @@ CREATE TABLE `p_item` (
 
 INSERT INTO `p_item` (`id_item`, `barcode`, `name`, `id_category`, `id_unit`, `price`, `stock`, `image`, `created_at`, `updated_at`) VALUES
 (14, 'A001', 'Paramex', 11, 2, 5000, 0, 'item-231114-e8cd559731.jpg', 2147483647, NULL),
-(15, 'A002', 'Ultraflu', 11, 2, 4500, 0, 'item-231114-897a3956e9.png', 2147483647, NULL);
+(16, 'A002', 'Bodrex', 11, 2, 4500, 0, 'item-231115-13f54d7b77.png', 2147483647, NULL),
+(17, 'A003', 'Ultraflu', 11, 2, 5000, 0, 'item-231115-5a22429cb0.png', 2147483647, NULL),
+(18, 'A004', 'Paracetamol', 11, 2, 3000, 0, 'item-231115-00ba39322c.png', 2147483647, NULL),
+(19, 'A005', 'Mylanta', 11, 2, 5000, 0, 'item-231115-f3a9d67f61.png', 2147483647, NULL),
+(20, 'A006', 'Amoxilin', 11, 2, 5500, 0, 'item-231115-8ae92307e7.png', 2147483647, NULL),
+(21, 'A007', 'Aspirin', 11, 2, 4500, 0, 'item-231115-d4f3eadc40.png', 2147483647, NULL),
+(22, 'A008', 'Lambucid', 11, 2, 6000, 0, 'item-231115-14a9d7dd80.png', 2147483647, NULL),
+(23, 'A009', 'Oskadon', 11, 2, 3500, 0, 'item-231115-508f6fb6d1.png', 2147483647, NULL),
+(24, 'A010', 'Diapet', 11, 2, 10000, 0, 'item-231115-4f6fa35d41.png', 2147483647, NULL),
+(25, 'B001', 'Entrostop', 11, 2, 6500, 0, 'item-231115-8fb7d5e114.png', 2147483647, NULL),
+(26, 'B002', 'Apel', 12, 3, 7000, 0, 'item-231115-f681494207.png', 2147483647, NULL);
 
 -- --------------------------------------------------------
 
@@ -111,7 +121,8 @@ CREATE TABLE `p_unit` (
 --
 
 INSERT INTO `p_unit` (`id_unit`, `name`, `created_at`, `update_at`) VALUES
-(2, 'Pcs', '2023-11-13 14:43:06', NULL);
+(2, 'Pcs', '2023-11-13 14:43:06', NULL),
+(3, 'Buah', '2023-11-15 13:23:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -137,6 +148,24 @@ INSERT INTO `supplier` (`id_supplier`, `name`, `phone`, `address`, `description`
 (1, 'PT Mandira Distra Abadi', '02129725200', 'Jl. Peta Barat No. 19, Kalideres, Jakarta 11830', NULL, '2023-10-13 11:35:01', NULL),
 (2, 'Dunia Cakrawala Abadi', '02122684586', 'jl.Hayam Wuruk No.127 Lantai GF2 Blok A26 No.6-7, Jakarta Barat, DKI Jakarta, Indonesia', NULL, '2023-10-13 11:38:43', NULL),
 (6, 'PT INDO TECHNO MEDIC', '099932323', 'Jln. Ring Road Selatan\r\n', 'somethin\' here\r\n', '2023-11-07 09:55:14', '2023-11-10 01:51:30');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `t_stock`
+--
+
+CREATE TABLE `t_stock` (
+  `id_stock` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  `type` enum('in','out') NOT NULL,
+  `detail` varchar(500) NOT NULL,
+  `id_supplier` int(11) NOT NULL,
+  `qty` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -201,6 +230,15 @@ ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
+-- Indeks untuk tabel `t_stock`
+--
+ALTER TABLE `t_stock`
+  ADD PRIMARY KEY (`id_stock`),
+  ADD KEY `id_item` (`id_item`),
+  ADD KEY `id_supplier` (`id_supplier`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -226,19 +264,25 @@ ALTER TABLE `p_category`
 -- AUTO_INCREMENT untuk tabel `p_item`
 --
 ALTER TABLE `p_item`
-  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT untuk tabel `p_unit`
 --
 ALTER TABLE `p_unit`
-  MODIFY `id_unit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_unit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `t_stock`
+--
+ALTER TABLE `t_stock`
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
@@ -256,6 +300,14 @@ ALTER TABLE `user`
 ALTER TABLE `p_item`
   ADD CONSTRAINT `p_item_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `p_category` (`id_category`),
   ADD CONSTRAINT `p_item_ibfk_2` FOREIGN KEY (`id_unit`) REFERENCES `p_unit` (`id_unit`);
+
+--
+-- Ketidakleluasaan untuk tabel `t_stock`
+--
+ALTER TABLE `t_stock`
+  ADD CONSTRAINT `t_stock_ibfk_1` FOREIGN KEY (`id_item`) REFERENCES `p_item` (`id_item`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_stock_ibfk_2` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`),
+  ADD CONSTRAINT `t_stock_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
